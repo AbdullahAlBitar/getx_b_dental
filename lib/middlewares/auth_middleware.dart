91def0b71aa444a347174e54cdc7f4af73f9a343
AbdullahAlBitar <abdullahalbitar2002@gmail.com>
2024-11-22 14:58:20 +0300

    changed auth middleware to check token validation

--- lib/middlewares/auth_middleware.dart
+++ lib/middlewares/auth_middleware.dart
@@ -1,6 +1,7 @@
 import 'package:getx_b_dental/main.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 
 class AuthMiddleware extends GetMiddleware {
 
@@ -9,7 +10,26 @@ class AuthMiddleware extends GetMiddleware {
 
   @override
   RouteSettings? redirect(String? route) {
-    if(sharedPreferences!.getString("jwt") != null) return const RouteSettings(name: "/home");
+    final token = sharedPreferences!.getString("jwt");
+    
+    if (token != null) {
+      try {
+        final response = http.get(
+          Uri.parse('$url/auth/token'),
+          headers: {
+            'Authorization': 'Bearer $token',
+          },
+        );
+
+        response.then((value) {
+          if (value.statusCode == 200) {
+            return const RouteSettings(name: "/home");
+          }
+        });
+      } catch (e) {
+        // Token validation failed
+      }
+    }
     return null;
   }
 }
